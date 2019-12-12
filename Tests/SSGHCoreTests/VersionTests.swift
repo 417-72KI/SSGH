d9e01d3d1c88e1a1a7510ca1fa6f9295a4b33393
417-72KI <417.72ki@gmail.com>
2019-12-12 16:51:28 +0900

    Rewrite version test as parameterized

--- Tests/SSGHCoreTests/VersionTests.swift
+++ Tests/SSGHCoreTests/VersionTests.swift
@@ -1,31 +1,42 @@
 import XCTest
+import ParameterizedTestUtil
 
 @testable import SSGHCore
 
 class VersionTests: XCTestCase {
+    override static func setUp() {
+        super.setUp()
+        ParameterizedTestUtil.needsDump = true
+    }
 
     func testStringLiteral() {
-        XCTAssertEqual("1.4", Version(1, 4, 0))
-        XCTAssertEqual("v2.8.9", Version(2, 8, 9))
-        XCTAssertEqual("2.8.2-alpha", Version(2, 8, 2, preRelease: "alpha"))
-        XCTAssertEqual("2.8.2-alpha+build234", Version(2, 8, 2, preRelease: "alpha", buildMetadata: "build234"))
-        XCTAssertEqual("2.8.2+build234", Version(2, 8, 2, buildMetadata: "build234"))
-        XCTAssertEqual("2.8.2-alpha.2.1.0", Version(2, 8, 2, preRelease: "alpha.2.1.0"))
+        runAll(
+            expect("1.4", equals: Version(1, 4, 0)),
+            expect("v2.8.9", equals: Version(2, 8, 9)),
+            expect("2.8.2-alpha", equals: Version(2, 8, 2, preRelease: "alpha")),
+            expect("2.8.2-alpha+build234", equals: Version(2, 8, 2, preRelease: "alpha", buildMetadata: "build234")),
+            expect("2.8.2+build234", equals: Version(2, 8, 2, buildMetadata: "build234")),
+            expect("2.8.2-alpha.2.1.0", equals: Version(2, 8, 2, preRelease: "alpha.2.1.0"))
+        )
     }
 
     func testDescription() {
-        XCTAssertEqual(Version(1, 4, 0).description, "1.4.0")
-        XCTAssertEqual(Version(2, 8, 9).description, "2.8.9")
-        XCTAssertEqual(Version(2, 8, 2, preRelease: "alpha").description, "2.8.2-alpha")
-        XCTAssertEqual(Version(2, 8, 2, preRelease: "alpha", buildMetadata: "build234").description, "2.8.2-alpha+build234")
-        XCTAssertEqual(Version(2, 8, 2, buildMetadata: "build234").description, "2.8.2+build234")
-        XCTAssertEqual(Version(2, 8, 2, preRelease: "alpha.2.1.0").description, "2.8.2-alpha.2.1.0")
+        runAll(
+            expect(Version(1, 4, 0).description, equals: "1.4.0"),
+            expect(Version(2, 8, 9).description, equals: "2.8.9"),
+            expect(Version(2, 8, 2, preRelease: "alpha").description, equals: "2.8.2-alpha"),
+            expect(Version(2, 8, 2, preRelease: "alpha", buildMetadata: "build234").description, equals: "2.8.2-alpha+build234"),
+            expect(Version(2, 8, 2, buildMetadata: "build234").description, equals: "2.8.2+build234"),
+            expect(Version(2, 8, 2, preRelease: "alpha.2.1.0").description, equals: "2.8.2-alpha.2.1.0")
+        )
     }
 
     func testCompare() {
-        XCTAssertTrue(Version(1, 4, 0) < "1.4.1")
-        XCTAssertTrue(Version(1, 5, 0) > "1.4.1")
-        XCTAssertTrue(Version(2, 0, 0) > "1.4.1")
-        XCTAssertTrue(Version(2, 0, 0, preRelease: "alpha") < "2.0.0")
+        runAll(
+            expect(Version(1, 4, 0), lessThan: "1.4.1"),
+            expect(Version(1, 5, 0), moreThan: "1.4.1"),
+            expect("2.0.0", moreThan: Version(1, 4, 1)),
+            expect(Version(2, 0, 0, preRelease: "alpha"), lessThan: "2.0.0")
+        )
     }
 }
