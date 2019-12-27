364e754384a44910edac8ac481e40c0e902a4797
417-72KI <417.72ki@gmail.com>
2019-12-27 14:23:54 +0900

    autocorrect

--- scripts/autocorrect.sh
+++ scripts/autocorrect.sh
@@ -7,4 +7,7 @@ if ! type "mint" > /dev/null; then
     brew install mint
 fi
 
-mint run swiftlint swiftlint autocorrect
+CORRECTED_FILES=$(mint run swiftlint swiftlint autocorrect 2> /dev/null | grep Corrected | awk '{ print $1 }' | sed -E 's/(.*\.swift):([0-9]*):([0-9]*)/\1/')
+
+git add $CORRECTED_FILES
+git commit -m 'Autocorrect by SwiftLint'
