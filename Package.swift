6fbdbda4f68b89e58a4ed087693981bcb7ed27b2
417-72KI <417.72ki@gmail.com>
2020-02-06 23:49:54 +0900

    Add `OctoKit` dependency

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.1.1")),
         .package(url: "https://github.com/ishkawa/APIKit.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")),
+        .package(name: "OctoKit", url: "https://github.com/nerdishbynature/octokit.swift", from: "0.11.0"),
         .package(url: "https://github.com/417-72KI/ParameterizedTestUtil.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
@@ -29,7 +30,7 @@ let package = Package(
         ),
         .target(
             name: "GitHubAPI",
-            dependencies: ["APIKit"]
+            dependencies: ["APIKit", "OctoKit"]
         ),
         .testTarget(
             name: "SSGHCoreTests",
@@ -42,7 +43,7 @@ let package = Package(
             name: "GitHubAPITests",
             dependencies: [
                 "GitHubAPI",
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs"),
+                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")
             ]
         )
     ],
