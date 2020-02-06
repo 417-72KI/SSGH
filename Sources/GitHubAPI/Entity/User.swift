a15206c94f2f7ad4735ffa6d886c2b348a94d5a6
417-72KI <417.72ki@gmail.com>
2020-02-07 00:56:19 +0900

    Replace `/user`

--- Sources/GitHubAPI/Entity/User.swift
+++ Sources/GitHubAPI/Entity/User.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OctoKit
 
 public struct User: Entity {
     public let login: String
@@ -7,4 +8,11 @@ public struct User: Entity {
 
 extension User: CustomStringConvertible {
     public var description: String { "@\(login)" }
+}
+
+extension User {
+    init(_ user: OctoKit.User) {
+        self.login = user.login ?? ""
+        self.publicRepos = UInt(user.numberOfPublicRepos ?? 0)
+    }
 }
