53894f2a498c4e8e5b8584dc0c02abbdea3b96a7
417-72KI <417.72ki@gmail.com>
2021-09-11 01:35:23 +0900

    implement dry-run mode

--- Sources/SSGH/main.swift
+++ Sources/SSGH/main.swift
@@ -16,7 +16,11 @@ struct SSGH: ParsableCommand {
     func run() throws {
         let gitHubToken = try gitHubToken ?? (try Environment.getValue(forKey: .gitHubToken))
         do {
-            try SSGHCore(target: target, gitHubToken: gitHubToken).execute()
+            try SSGHCore(
+                target: target,
+                gitHubToken: gitHubToken,
+                dryRunMode: dryRunMode
+            ).execute()
         } catch {
             dumpError(error)
             Self.exit(withError: error)
