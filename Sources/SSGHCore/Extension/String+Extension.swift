7baa1eda39f681955f97d69ea4be6daef312b0ec
417-72KI <417.72ki@gmail.com>
2021-09-11 01:53:57 +0900

    disable `legacy_objc_type` on using `NSString`

--- Sources/SSGHCore/Extension/String+Extension.swift
+++ Sources/SSGHCore/Extension/String+Extension.swift
@@ -2,6 +2,7 @@ import Foundation
 
 extension String {
     func substring(_ range: NSRange) -> String {
+        // swiftlint:disable:next legacy_objc_type
         (self as NSString).substring(with: range)
     }
 }
