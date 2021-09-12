26bd901b67c01b5adea3394657773fe4ad5d6062
417-72KI <417.72ki@gmail.com>
2021-09-12 21:47:27 +0900

    fix `legacy_objc_type`

--- Sources/SSGHCore/Extension/String+Extension.swift
+++ Sources/SSGHCore/Extension/String+Extension.swift
@@ -2,13 +2,21 @@ import Foundation
 
 extension String {
     var lastPathComponent: String {
-        (self as NSString).lastPathComponent
+        nsString.lastPathComponent
     }
 }
 
 extension String {
     func substring(_ range: NSRange) -> String {
+        nsString.substring(with: range)
+    }
+}
+
+// MARK: -
+private extension String {
+    // swiftlint:disable:next legacy_objc_type
+    var nsString: NSString {
         // swiftlint:disable:next legacy_objc_type
-        (self as NSString).substring(with: range)
+        self as NSString
     }
 }
