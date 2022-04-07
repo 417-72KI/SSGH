79a2304320844f35dbac40e4f39a00380a83281d
417-72KI <417.72ki@gmail.com>
2022-04-07 16:32:36 +0900

    Replace `/releases`

--- Sources/GitHubAPI/Entity/Release.swift
+++ Sources/GitHubAPI/Entity/Release.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OctoKit
 
 public struct Release: Entity {
     public let url: URL
@@ -6,4 +7,14 @@ public struct Release: Entity {
     public let tagName: String
     public let prerelease: Bool
     public let draft: Bool
+}
+
+extension Release {
+    init(_ release: OctoKit.Release) {
+        self.url = release.url
+        self.name = release.name
+        self.tagName = release.tagName
+        self.prerelease = release.prerelease
+        self.draft = release.draft
+    }
 }
--- Sources/GitHubAPI/GetReleases.swift
+++ Sources/GitHubAPI/GetReleases.swift
@@ -1,29 +1,19 @@
-import APIKit
 import Foundation
+import OctoKit
 
 extension GitHubClient {
     public func getReleases(for userId: String, repo: String) -> Result<[Release], GitHubClient.Error> {
-        sendSync(Releases.Get(owner: userId, repo: repo))
+        var result: Result<[OctoKit.Release], Swift.Error>!
+        let semaphore = DispatchSemaphore(value: 0)
+        octoKit.listReleases(owner: userId, repository: repo) {
+            result = $0.result
+            semaphore.signal()
+        }
+        semaphore.wait()
+        return result.map { $0.map(Release.init) }
             .mapError {
-                if case let .responseError(re) = $0,
-                    let responseError = re as? APIKit.ResponseError,
-                    case let .unacceptableStatusCode(code) = responseError,
-                    code == 404 { return .repoNotFound("\(userId)/\(repo)") }
+                if ($0 as NSError).code == 404 { return .repoNotFound("\(userId)/\(repo)") }
                 return .other($0)
             }
-    }
-}
-
-extension GitHubClient {
-    enum Releases {
-        struct Get: Request {
-            typealias Response = [Release]
-
-            let owner: String
-            let repo: String
-
-            var method: HTTPMethod { .get }
-            var path: String { "/repos/\(owner)/\(repo)/releases" }
-        }
     }
 }
