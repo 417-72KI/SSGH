da2750c1b83c3dfe37e34b8ecca3c0ebbcd06eab
417-72KI <417.72ki@gmail.com>
2022-04-14 08:51:08 +0900

    replace `github-release` with `gh`

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+EXECUTABLE_NAME=$1
 APPLICATION_INFO_FILE='Sources/SSGHCore/Common/ApplicationInfo.swift'
 
 if [ `git symbolic-ref --short HEAD` != 'main' ]; then
@@ -19,44 +20,23 @@ if [ "$(git status -s | grep .swift | grep -v ApplicationInfo.swift)" != '' ]; t
     exit 1
 fi
 
-if ! type "github-release" > /dev/null; then
-    echo '`github-release` not found. Install'
-    go get github.com/github-release/github-release
+if ! type "gh" > /dev/null; then
+    echo '`gh` not found. Install'
+    brew install gh
 fi
 
 cd $(git rev-parse --show-toplevel)
 
 # Version
-TAG=$(cat "${APPLICATION_INFO_FILE}" | grep version | awk '{ print $NF }' | sed -E 's/Version\(\"(.*)\"\)/\1/')
+TAG=$(swift run ssgh --version 2>/dev/null)
 if [ "$(git tag | grep ${TAG})" != '' ]; then
     echo "\e[31mTag: '${TAG}' already exists.\e[m"
     exit 1
 fi
 
-# Build
-EXECUTABLE_NAME=$1
-rm -f .build/${EXECUTABLE_NAME}.zip
-swift build -c release -Xswiftc -suppress-warnings
-zip -j .build/${EXECUTABLE_NAME}.zip .build/release/${EXECUTABLE_NAME} LICENSE
-
 # TAG
 git commit -m "Bump version to ${TAG}" "${APPLICATION_INFO_FILE}"
-git tag ${TAG}
-git push origin main "${TAG}"
+git push origin main
 
-export GITHUB_TOKEN=$SSGH_TOKEN
-
-# GitHub Release
-github-release release \
-    --user 417-72KI \
-    --repo SSGH \
-    --tag ${TAG}
-
-github-release upload \
-    --user 417-72KI \
-    --repo SSGH \
-    --tag ${TAG} \
-    --name "${EXECUTABLE_NAME}.zip" \
-    --file .build/${EXECUTABLE_NAME}.zip
-
-
+# Release
+gh release create ${TAG}
