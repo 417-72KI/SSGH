5947f953d481589839f4b9ab75fb37463e4dcb69
417-72KI <417.72ki@gmail.com>
2022-04-18 03:50:06 +0900

    fix

--- Sources/SSGH/SSGH.swift
+++ Sources/SSGH/SSGH.swift
@@ -11,13 +11,13 @@ enum Main: AsyncMainProtocol {
 
 struct SSGH: AsyncParsableCommand {
     @Argument(help: "GitHub user name to give stars.")
-    var target: String
+    var targets: [String]
 
     @Option(name: [.customLong("github-token"), .customShort("t")],
             help: "GitHub Token to give stars. If not set, use `SSGH_TOKEN` in environment.")
     var gitHubToken: String?
 
-    @Flag(name: .short, help: "dry-run mode. Only fetch lists to give stars.")
+    @Flag(name: [.customLong("dry-run"), .short], help: "dry-run mode. Only fetch lists to give stars.")
     var dryRunMode = false
 }
 #else
